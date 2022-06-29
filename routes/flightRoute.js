40df601a9f3f6dcb2029be987a754d999ba21591
Sodiq Farhan <sodiqfarhan360@gmail.com>
2022-06-29 10:49:18 +0100

    Add all routes to achieve various tasks on the api

--- routes/flightRoute.js
+++ routes/flightRoute.js
@@ -3,7 +3,13 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/flightController');
 
-router.get('/', controller.example)
+router
+    .get('/', controller.welcome)
+    .get('/flights', controller.getBookings)
+    .post('/flights', controller.bookFlight)
+    .get('/flights/:id', controller.getFlight)
+    .put('/flights/:id', controller.updateFlight)
+    .delete('/flights/:id', controller.deleteFlight);
 
 module.exports = router;
 
